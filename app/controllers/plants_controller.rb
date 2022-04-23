3d4cc8fa5c9778d49625dd367dffca9f5855abb7
Eric Hafvenstein <81645330+theFlexOne@users.noreply.github.com>
2022-04-23 01:22:39 -0500

    complete

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -3,19 +3,39 @@ class PlantsController < ApplicationController
   # GET /plants
   def index
     plants = Plant.all
-    render json: plants
+    render json: plants, status: :ok
   end
 
   # GET /plants/:id
   def show
     plant = Plant.find_by(id: params[:id])
-    render json: plant
+    render json: plant, status: :ok
   end
 
   # POST /plants
   def create
     plant = Plant.create(plant_params)
     render json: plant, status: :created
+  end
+
+  def update
+    plant = Plant.find_by(id: params[:id])
+    if plant
+      plant.update(plant_params)
+      render json: plant, status: :accepted
+    else
+      render json: { error: "Plant not found" }, status: :not_found
+    end
+  end
+
+  def destroy
+    plant = Plant.find_by(id: params[:id])
+    if plant
+      plant.destroy
+      head :no_content
+    else
+      render json: { error: "Plant not found" }, status: :not_found
+    end
   end
 
   private
